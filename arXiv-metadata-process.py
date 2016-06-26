20a9c2552952cc2f9d5a333046c8a3eb9195119b
rocksonchang <chang.rockson@gmail.com>
2016-06-26 19:39:14 -0400

    Update
    Some preliminary processing of data, looking at word frequency as a
    function of time

--- arXiv-metadata-process.py
+++ arXiv-metadata-process.py
@@ -25,8 +25,13 @@ from xml.dom import minidom
 #from xml.dom.minidom import Document
 from itertools import chain
 from datetime import datetime
+import itertools
 import time
-## throwing down some code I don't yet understand regarding non-ascii characters in the database
+#import numpy as np
+from math import ceil
+## Using some simple functions from http://programminghistorian.org/lessons/counting-frequencies
+import obo
+## handling of non-ascii characters in the database
 # http://chase-seibert.github.io/blog/2014/01/12/python-unicode-console-output.html
 import sys
 import codecs
@@ -34,18 +39,24 @@ sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 sys.stderr = codecs.getwriter('utf8')(sys.stderr)
 
 
+#################################################################################################
+#################################################################################################
+## load data
 #creators=[]
 addys=[]
 titles=[]
 descriptions=[]
 dates=[]
-for year in range(1992,2017):
-	fname ='arXiv-meta-{}.xml'.format(year)	
+
+years=range(1992,1999)
+NRecYear=[0]*(len(years))	
+
+for x in range(len(years)):	
+	fname ='arXiv-meta-{}.xml'.format(years[x])	
 	## parse XML data
 	data = minidom.parse(fname)		
-	NEntries=0		
 	for rec in data.getElementsByTagName('record'):			
-		NEntries+=1
+		NRecYear[x]+=1
 		## extract data
 		#t_creators=()
 		#for c in rec.getElementsByTagName("dc:creator"):			
@@ -65,13 +76,106 @@ for year in range(1992,2017):
 		titles.append(rec.getElementsByTagName("title")[0].firstChild.data)
 		descriptions.append(rec.getElementsByTagName("description")[0].firstChild.data)
 		dates.append(rec_dates[0]) # take date first submitted as date.
-	print ('Loading year: {}; Num. entries: {}'.format(year,NEntries))
-'''
+	print ('Loading year: {}; Num. entries: {}'.format(years[x],NRecYear[x]))
+
+
 ## output some stuff to check	
-print('\n')		
-for i in range(len(titles)):
-	print(titles[i])
-	print(dates[i])
-	print(descriptions[i])
-	print('\n')	
+#print('\n')		
+#for i in range(len(titles)): print(titles[i]);	print(dates[i]); print(descriptions[i]); print('\n');
+
+
+#################################################################################################
+#################################################################################################
+## yearly blocks
 '''
+print('\n')
+NRec=sum(NRecYear)
+NRecRunning=0
+for x in range(len(years)):	
+	fulltextYear=''	
+	for i in range(NRecYear[x]):
+		rec=i+NRecRunning				
+		desc=descriptions[rec]
+		title=titles[rec]
+		text=desc+title
+		fulltextYear=fulltextYear+text	
+	NRecRunning=NRecRunning+NRecYear[x]
+
+	
+
+	desc_fullwordlist = obo.stripNonAlphaNum(fulltextYear)
+	desc_wordlist = obo.removeStopwords(desc_fullwordlist,obo.stopwords)	
+	desc_dictionary = obo.wordListToFreqDict(desc_wordlist)
+	desc_sorteddict = obo.sortFreqDict(desc_dictionary)
+	
+	print ('Year: {}; Num. entries: {}'.format(years[x],NRecYear[x]))
+
+	#x = itertools.islice(desc_sorteddict.items(), 0, 10)
+
+	for s in desc_sorteddict[:30]: 	
+		print(str(s))
+	print('\n')
+'''
+
+#################################################################################################
+#################################################################################################
+## Quarterly blocks
+print('\n')
+NRec=sum(NRecYear)
+top100=[]
+NRecRunning=0
+year0=1992
+NRecQuarter=[0]*25*4
+for x in range(len(years)):	
+	fulltextQuarter=['']*4
+	for i in range(NRecYear[x]):
+		rec=i+NRecRunning				
+		desc=descriptions[rec]
+		
+		title=titles[rec]
+		text=title+desc
+
+		date=datetime.strptime(dates[rec],'%Y-%m-%d')		
+		Q=int(ceil(date.month/3)-1)
+		ind = 4*(date.year-year0)+Q
+		NRecQuarter[ind]+=1
+
+		fulltextQuarter[Q]=fulltextQuarter[Q]+text		
+		
+	NRecRunning=NRecRunning+NRecYear[x]
+	
+
+	for q in range(4):
+		desc_fullwordlist = obo.stripNonAlphaNum(fulltextQuarter[q])
+		desc_wordlist = obo.removeStopwords(desc_fullwordlist,obo.stopwords)	
+		desc_dictionary = obo.wordListToFreqDict(desc_wordlist)
+		desc_sorteddict = obo.sortFreqDict(desc_dictionary)
+
+		top100.append(desc_sorteddict[:300])
+		'''
+		print ('Year: {}; Quarter: Q{}; Num. entries: {}'.format(years[x],q+1,len(fulltextQuarter[q])))
+		for s in desc_sorteddict[:40]: 	print(str(s))
+		print('\n')
+		'''
+
+#with open('obj/'+ name + '.pkl', 'wb') as f:
+	#pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+year0=1992
+N=[0]*len(top100)
+for q in range(len(top100)):
+	year  = year0  + int(ceil((q+1.)/4)-1)
+	print('Year: {}; Quarter: Q{}; Num. entries: {}').format(year, q%4+1, NRecQuarter[q])
+	top100quarter=top100[q]
+	for s in top100quarter[:10]: print(str(s))
+	print('\n')
+
+	dictQuarter=dict(top100quarter)
+	invDict = {v: k for k, v in dictQuarter.items()}
+	
+	word='bose'.lower()
+	
+	if word in invDict: N[q] = invDict.get(word); 
+	else: N[q]=0
+
+print(N)
---
+++ obo.py
@@ -0,0 +1,127 @@
+# obo.py
+
+stopwords = ['a', 'about', 'above', 'across', 'after', 'afterwards']
+stopwords += ['again', 'against', 'all', 'almost', 'alone', 'along']
+stopwords += ['already', 'also', 'although', 'always', 'am', 'among']
+stopwords += ['amongst', 'amoungst', 'amount', 'an', 'and', 'another']
+stopwords += ['any', 'anyhow', 'anyone', 'anything', 'anyway', 'anywhere']
+stopwords += ['are', 'around', 'as', 'at', 'back', 'be', 'became']
+stopwords += ['because', 'become', 'becomes', 'becoming', 'been']
+stopwords += ['before', 'beforehand', 'behind', 'being', 'below']
+stopwords += ['beside', 'besides', 'between', 'beyond', 'bill', 'both']
+stopwords += ['bottom', 'but', 'by', 'call', 'can', 'cannot', 'cant']
+stopwords += ['co', 'computer', 'con', 'could', 'couldnt', 'cry', 'de']
+stopwords += ['describe', 'detail', 'did', 'do', 'done', 'down', 'due']
+stopwords += ['during', 'each', 'eg', 'eight', 'either', 'eleven', 'else']
+stopwords += ['elsewhere', 'empty', 'enough', 'etc', 'even', 'ever']
+stopwords += ['every', 'everyone', 'everything', 'everywhere', 'except']
+stopwords += ['few', 'fifteen', 'fifty', 'fill', 'find', 'fire', 'first']
+stopwords += ['five', 'for', 'former', 'formerly', 'forty', 'found']
+stopwords += ['four', 'from', 'front', 'full', 'further', 'get', 'give']
+stopwords += ['go', 'had', 'has', 'hasnt', 'have', 'he', 'hence', 'her']
+stopwords += ['here', 'hereafter', 'hereby', 'herein', 'hereupon', 'hers']
+stopwords += ['herself', 'him', 'himself', 'his', 'how', 'however']
+stopwords += ['hundred', 'i', 'ie', 'if', 'in', 'inc', 'indeed']
+stopwords += ['interest', 'into', 'is', 'it', 'its', 'itself', 'keep']
+stopwords += ['last', 'latter', 'latterly', 'least', 'less', 'ltd', 'made']
+stopwords += ['many', 'may', 'me', 'meanwhile', 'might', 'mill', 'mine']
+stopwords += ['more', 'moreover', 'most', 'mostly', 'move', 'much']
+stopwords += ['must', 'my', 'myself', 'name', 'namely', 'neither', 'never']
+stopwords += ['nevertheless', 'next', 'nine', 'no', 'nobody', 'none']
+stopwords += ['noone', 'nor', 'not', 'nothing', 'now', 'nowhere', 'of']
+stopwords += ['off', 'often', 'on','once', 'one', 'only', 'onto', 'or']
+stopwords += ['other', 'others', 'otherwise', 'our', 'ours', 'ourselves']
+stopwords += ['out', 'over', 'own', 'part', 'per', 'perhaps', 'please']
+stopwords += ['put', 'rather', 're', 's', 'same', 'see', 'seem', 'seemed']
+stopwords += ['seeming', 'seems', 'serious', 'several', 'she', 'should']
+stopwords += ['show', 'side', 'since', 'sincere', 'six', 'sixty', 'so']
+stopwords += ['some', 'somehow', 'someone', 'something', 'sometime']
+stopwords += ['sometimes', 'somewhere', 'still', 'such', 'system', 'take']
+stopwords += ['ten', 'than', 'that', 'the', 'their', 'them', 'themselves']
+stopwords += ['then', 'thence', 'there', 'thereafter', 'thereby']
+stopwords += ['therefore', 'therein', 'thereupon', 'these', 'they']
+stopwords += ['thick', 'thin', 'third', 'this', 'those', 'though', 'three']
+stopwords += ['three', 'through', 'throughout', 'thru', 'thus', 'to']
+stopwords += ['together', 'too', 'top', 'toward', 'towards', 'twelve']
+stopwords += ['twenty', 'two', 'un', 'under', 'until', 'up', 'upon']
+stopwords += ['us', 'very', 'via', 'was', 'we', 'well', 'were', 'what']
+stopwords += ['whatever', 'when', 'whence', 'whenever', 'where']
+stopwords += ['whereafter', 'whereas', 'whereby', 'wherein', 'whereupon']
+stopwords += ['wherever', 'whether', 'which', 'while', 'whither', 'who']
+stopwords += ['whoever', 'whole', 'whom', 'whose', 'why', 'will', 'with']
+stopwords += ['within', 'without', 'would', 'yet', 'you', 'your']
+stopwords += ['yours', 'yourself', 'yourselves']
+## stop words particular to quant-ph abstracts
+stopwords += ['quantum', 'classical', 'theory','state','states','show','new']
+
+
+def stripTags(pageContents):
+    startLoc = pageContents.find("<hr/><h2>")
+ 
+    pageContents = pageContents[startLoc:]
+
+    inside = 0
+    text = ''
+    for char in pageContents:
+        if char == '<':
+            inside = 1
+        elif (inside == 1 and char == '>'):
+            inside = 0
+        elif inside == 1:
+            continue
+        else:
+            text += char
+    return text
+
+
+
+
+# Given a text string, remove all non-alphanumeric
+# characters (using Unicode definition of alphanumeric).
+
+# Rockson: I've hacked the shit out of this to make areally messy hack to treat latex umlauts...
+# Now it converts html codes into text, then removes \\" string from text.
+# Converts everything to lower case
+# Splits into a list
+
+def stripNonAlphaNum(text):
+   
+    from HTMLParser import HTMLParser
+    h = HTMLParser()
+    text2=h.unescape(text).encode()
+    
+    REMOVE_LIST = ['\\"','\\it']
+    REPLACE_LIST = ['-']
+    for remove in REMOVE_LIST:
+        text2=text2.replace(remove,'')
+    for replace in REPLACE_LIST:
+        text2=text2.replace(replace,' ')
+    out=text2.lower()
+    return out.split()
+
+    
+
+# Given a list of words, return a dictionary of
+# word-frequency pairs.
+
+def wordListToFreqDict(wordlist):
+    wordfreq = [wordlist.count(p) for p in wordlist]
+    return dict(zip(wordlist,wordfreq))
+
+
+# Sort a dictionary of word-frequency pairs in
+# order of descending frequency.
+
+def sortFreqDict(freqdict):
+    aux = [(freqdict[key], key) for key in freqdict]
+    aux.sort()
+    aux.reverse()
+    return aux
+
+
+# Given a list of words, remove any that are
+# in a list of stop words.
+
+def removeStopwords(wordlist, stopwords):
+    return [w for w in wordlist if w not in stopwords]
+
